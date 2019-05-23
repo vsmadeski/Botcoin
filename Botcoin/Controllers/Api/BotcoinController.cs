4da21dab6cc2f04fc9e85168e6e27a8e11903e80
Vinicius Smadeski <vinicius.js94@gmail.com>
2019-05-22 23:55:04 -0300

    Implement Get Prices feature

--- Botcoin/Controllers/Api/BotcoinController.cs
+++ Botcoin/Controllers/Api/BotcoinController.cs
@@ -1,17 +1,37 @@
 ﻿using System;
+using Botcoin.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Botcoin.Utils;
 
 namespace Botcoin.Controllers.Api
 {
     public class BotcoinController : ApiController
     {
-        public IHttpActionResult GetPrices(string actionName)
+        public async Task<IHttpActionResult> GetPrices(string botcoinParams)
         {
-            return Ok();
+            var options = JsonConvert.DeserializeObject<BotcoinOptions>(botcoinParams);
+
+            if(options.ActionName == ActionNames.GetPrices && !string.IsNullOrWhiteSpace(options.SelectedCoin))
+            {
+                using (var client = new HttpClient())
+                {
+                    var uri = new Uri(@"https://www.mercadobitcoin.net/api/" + options.SelectedCoin + @"/ticker/");
+
+                    var response = await client.GetAsync(uri);
+
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    return Ok(result);
+                }
+            }
+
+            return NotFound();
         }
     }
 }
---
+++ Botcoin/Models/BotcoinOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Botcoin.Models
+{
+    public class BotcoinOptions
+    {
+        public string ActionName { get; set; }
+
+        public string SelectedCoin { get; set; }
+    }
+}
